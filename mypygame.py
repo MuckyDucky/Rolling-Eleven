0130ba4733dc4e10b8551061d150e72780c6e9e2
Jimin Yook <sprkal129@gmail.com>
2018-04-18 15:07:19 +0900

    version with intro

--- mypygame.py
+++ mypygame.py
@@ -11,14 +11,19 @@ cSec=0
 cFrame=0
 FPS=0
 
+game_title="FIFAL Fantasy"
+
 
 
 
 fps_font=pygame.font.Font("C:\\Windows\\Fonts\\Verdana.ttf",20)
+msg_font=pygame.font.Font("C:\\Windows\\Fonts\\Verdana.ttf", 20)#dont assume font exists. change later
 
 sky = pygame.image.load("graphics\\sky.png")
 Sky=pygame.Surface(sky.get_size(), pygame.HWSURFACE)
 Sky.blit(sky, (0,0))
+
+
 del sky
 
 def show_fps():
@@ -28,7 +33,7 @@ def show_fps():
 def create_window():
     global window, window_height, window_width, window_title
     window_width, window_height=800,600
-    window_title="FIFAL Fantasy"
+    window_title=game_title
     pygame.display.set_caption(window_title)
     window=pygame.display.set_mode((window_width, window_height), pygame.HWSURFACE|pygame.DOUBLEBUF)
 
@@ -43,70 +48,105 @@ def count_fps():
         cSec=time.strftime("%S")
         if FPS > 0:
             deltatime=1 / FPS
-        
 
+def message_display(msg):
+    text=msg_font.render(msg, True, Color.Red)
+    text_rect=text.get_rect(center=(window_width/2, window_height/2))
     
-
-create_window()    
-
-isRunning=True
-
+    window.blit(text,text_rect)
 
 #EVENTS LOOP
-while isRunning:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            isRunning = False
+def main_loop():
+    isRunning=True    
+    while isRunning:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                isRunning = False
+                
 
-        if event.type == pygame.KEYDOWN: #if key pressed
-            if event.key == pygame.K_w:
-                Globals.camera_move=1
-            elif event.key == pygame.K_s:
-                Globals.camera_move=2
-            elif event.key == pygame.K_a:
-                Globals.camera_move=3
-            elif event.key == pygame.K_d:
-                Globals.camera_move=4
+            if event.type == pygame.KEYDOWN: #if key pressed
+                if event.key == pygame.K_w:
+                    Globals.camera_move=1
+                elif event.key == pygame.K_s:
+                    Globals.camera_move=2
+                elif event.key == pygame.K_a:
+                    Globals.camera_move=3
+                elif event.key == pygame.K_d:
+                    Globals.camera_move=4
+                    
+            elif event.type == pygame.KEYUP:
+                Globals.camera_move=0
+                    
+                    
                 
-        elif event.type == pygame.KEYUP:
-            Globals.camera_move=0
-                
-                
+
+        #LOGIC
+        if Globals.camera_move==1:
+            Globals.camera_y += 100 * deltatime
+        elif Globals.camera_move == 2:
+            Globals.camera_y -= 100 * deltatime
+        elif Globals.camera_move == 3:
+            Globals.camera_x += 100 * deltatime
+        elif Globals.camera_move == 4:
+            Globals.camera_x -= 100 * deltatime         
+
             
 
-    #LOGIC
-    if Globals.camera_move==1:
-        Globals.camera_y += 100 * deltatime
-    elif Globals.camera_move == 2:
-        Globals.camera_y -= 100 * deltatime
-    elif Globals.camera_move == 3:
-        Globals.camera_x += 100 * deltatime
-    elif Globals.camera_move == 4:
-        Globals.camera_x -= 100 * deltatime         
+        
+        # RENDER GRAPHICS          
+        window.blit(Sky, (0,0))
 
+        #-Render Simple Grid
+        for x in range (0,window_width, Tiles.Size):
+            for y in range(0, window_height, Tiles.Size):
+                window.blit(Tiles.Grass,(x,y))
         
 
-    
-    # RENDER GRAPHICS          
-    window.blit(Sky, (0,0))
+        
+        show_fps()
+        
+        pygame.display.update()
 
-    #-Render Simple Grid
-    for x in range (0,640, Tiles.Size):
-        for y in range(0, 480, Tiles.Size):
-            window.blit(Tiles.Grass,(x+Globals.camera_x,y+Globals.camera_y))
-    
+        count_fps()
+    pygame.quit()
+    sys.exit()    
 
-    
-    show_fps()        
+
+        
+def game_intro ():
+    intro = True
+    window.fill(Color.White)
+    message_display('Welcome to '+game_title+'.\n Press Space to start game')
     pygame.display.update()
-
-    count_fps()
-
+    while intro:
+        for event in pygame.event.get():
+            if event.type==pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    intro=False                    
+                    main_loop()
+                    
+                            
+                            
+        
     
     
+            
+    
 
-pygame.quit()
-sys.exit()
+create_window()
+game_intro()
+
+
+
+
+
+        
+        
+
+        
+        
+
+
 
 
     
---
+++ scripts/players.py
@@ -0,0 +1,30 @@
+import pygame
+
+class Player:
+    def __init__(self, name):
+        self.name=name
+        self.money=0
+        self.score=0
+        self.roster=[]
+        self.diceVal=0
+    def score_goal():
+        self.score+=1
+    def add_to_roster(playername):
+        roster.append(playername) #fix later
+    def assignDiceVal(diceval):
+        self.diceVal=diceval
+
+class Athlete:
+    def __init__(self, name, atk, dfns, stm):
+        self.name=name
+        self.atk=atk
+        self.dfns=dfns
+        self.stm=stm
+    def reduceStm(num):
+        self.stm-=num
+        
+        
+        
+            
+        
+        
